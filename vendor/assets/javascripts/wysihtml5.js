78f44737137d6fbdbc21200b829a056eef202b63
ashaegupta <ashaegupta@gmail.com>
2012-11-09 14:59:52 -0500

    Added back in 'wysihtml5 message'

--- vendor/assets/javascripts/wysihtml5.js
+++ vendor/assets/javascripts/wysihtml5.js
@@ -9448,6 +9448,10 @@ wysihtml5.views.Textarea = wysihtml5.views.View.extend(
         }
       });
 
+      try {
+        console.log("Heya! This page is using wysihtml5 for rich text editing. Check out https://github.com/xing/wysihtml5");
+        } catch(e) {}
+
     },
     
     isCompatible: function() {
