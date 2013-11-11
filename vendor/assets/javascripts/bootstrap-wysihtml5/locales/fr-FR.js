40511848d59ab8f11a8b8725e3506fd360636ec6
Loyer Sebastien <loyer.sebastien@gmail.com>
2013-11-12 00:34:44 +0100

    update fr-FR locale

--- vendor/assets/javascripts/bootstrap-wysihtml5/locales/fr-FR.js
+++ vendor/assets/javascripts/bootstrap-wysihtml5/locales/fr-FR.js
@@ -7,7 +7,10 @@
               normal: "Texte normal",
               h1: "Titre 1",
               h2: "Titre 2",
-              h3: "Titre 3"
+              h3: "Titre 3",
+              h4: "Titre 4",
+              h5: "Titre 5",
+              h6: "Titre 6"
         },
         emphasis: {
               bold: "Gras",
