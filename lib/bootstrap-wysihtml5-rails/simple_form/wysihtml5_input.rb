d1a7fd09c2ad93b25366154c5332293737a73911
Cartier Alexandre <fiyorden@gmail.com>
2014-11-05 16:22:05 +0100

    Update wysihtml5_input.rb

--- lib/bootstrap-wysihtml5-rails/simple_form/wysihtml5_input.rb
+++ lib/bootstrap-wysihtml5-rails/simple_form/wysihtml5_input.rb
@@ -1,10 +1,12 @@
 class Wysihtml5Input < SimpleForm::Inputs::TextInput
   def input(wrapper_options)
     idf = "#{lookup_model_names.join("_")}_#{reflection_or_attribute_name}"
-    script = template.content_tag(:script, type: 'text/javascript') do
-      "$('textarea[id=#{idf}]').wysihtml5();".html_safe
-    end
+    script = template.content_for(:javascript) do
+              template.content_tag(:script, type: 'text/javascript') do
+                "$('textarea[id=#{idf}]').wysihtml5();".html_safe
+              end
+            end
 
-    super + content_for(:javascript) do  script  end
+    super + script
   end
 end
