be3ea968efdffc845104616c97b74b66e7bbc3ad
Nerian <siotopo@gmail.com>
2015-01-21 10:29:13 +0100

    bump up

--- lib/bootstrap-wysihtml5-rails/version.rb
+++ lib/bootstrap-wysihtml5-rails/version.rb
@@ -1,6 +1,5 @@
 module BootstrapWysihtml5Rails
   module Rails
-    # Change to follow bootstrap3-wysihtml5-bower version as soon as they we can.
-    VERSION = "0.3.3.1"
+    VERSION = "0.3.3.2"
   end
 end
