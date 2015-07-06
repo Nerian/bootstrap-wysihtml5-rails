7e1f878dd04a696f9e095151d4270209e9bf6987
skv <skv-headless@yandex.ru>
2015-07-06 16:12:31 +0300

    ability to throw params with simple form

--- lib/bootstrap-wysihtml5-rails/simple_form/wysihtml5_input.rb
+++ lib/bootstrap-wysihtml5-rails/simple_form/wysihtml5_input.rb
@@ -2,7 +2,7 @@ class Wysihtml5Input < SimpleForm::Inputs::TextInput
   def input(wrapper_options)
     idf = "#{lookup_model_names.join("_")}_#{reflection_or_attribute_name}"
     script = template.content_tag(:script, type: 'text/javascript') do
-      "$('textarea[id=#{idf}]').wysihtml5();".html_safe
+      "$('textarea[id=#{idf}]').wysihtml5(#{options[:wysihtml5].to_json});".html_safe
     end
 
     super + script
